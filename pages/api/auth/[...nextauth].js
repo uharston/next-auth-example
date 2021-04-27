3a3031b4efe2fd97cf15865a40ba25b231b8d0f0
Uriah Harston <uharston@gmail.com>
2021-04-26 23:04:41 -0500

    Configure b2c login

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -3,43 +3,46 @@ import Providers from "next-auth/providers"
 
 // For more information on each option (and a full list of options) go to
 // https://next-auth.js.org/configuration/options
+
+const tenantName = process.env.AZURE_TENANT_NAME;
+const tenantId = process.env.AZURE_TENANT_ID;
+const userFlow = process.env.USER_FLOW;
 export default NextAuth({
   // https://next-auth.js.org/configuration/providers
   providers: [
-    Providers.Email({
-      server: process.env.EMAIL_SERVER,
-      from: process.env.EMAIL_FROM,
-    }),
-    Providers.Apple({
-      clientId: process.env.APPLE_ID,
-      clientSecret: {
-        appleId: process.env.APPLE_ID,
-        teamId: process.env.APPLE_TEAM_ID,
-        privateKey: process.env.APPLE_PRIVATE_KEY,
-        keyId: process.env.APPLE_KEY_ID,
-      },
-    }),
-    Providers.Auth0({
-      clientId: process.env.AUTH0_ID,
-      clientSecret: process.env.AUTH0_SECRET,
-      domain: process.env.AUTH0_DOMAIN,
-    }),
-    Providers.Facebook({
-      clientId: process.env.FACEBOOK_ID,
-      clientSecret: process.env.FACEBOOK_SECRET,
-    }),
+
     Providers.GitHub({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
     }),
-    Providers.Google({
-      clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET,
-    }),
-    Providers.Twitter({
-      clientId: process.env.TWITTER_ID,
-      clientSecret: process.env.TWITTER_SECRET,
-    }),
+    Providers.AzureADB2C({
+      clientId: process.env.AZURE_CLIENT_ID,
+      clientSecret: process.env.AZURE_AUTH_CLIENT_SECRET,
+      scope: 'offline_access openid',
+      tenantId: tenantId,
+      id: 'azureb2c',
+      name: 'Azure B2C',
+      type: 'oauth',
+      version: '2.0',
+      idToken: true,
+      state: false,
+      params: {
+        grant_type: 'authorization_code',
+      },
+      profile: profile => {
+        console.log(profile);
+        return {
+          id: profile.oid,
+          email:profile.emails[0]
+        };
+      },
+      accessTokenUrl: `https://${tenantName}.b2clogin.com/${tenantName}.onmicrosoft.com/${userFlow}/oauth2/v2.0/token`,
+
+      // requestTokenUrl: `https://login.microsoftonline.com/${process.env.AUTH_TENANT_GUID}/oauth2/v2.0/token`,
+      authorizationUrl: `https://${tenantName}.b2clogin.com/${tenantName}.onmicrosoft.com/${userFlow}/oauth2/v2.0/authorize?response_type=code+id_token&response_mode=form_post`,
+      profileUrl: 'https://graph.microsoft.com/oidc/userinfo',
+    })
+
   ],
   // Database optional. MySQL, Maria DB, Postgres and MongoDB are supported.
   // https://next-auth.js.org/configuration/databases
@@ -47,7 +50,7 @@ export default NextAuth({
   // Notes:
   // * You must install an appropriate node_module for your database
   // * The Email provider requires a database (OAuth providers do not)
-  database: process.env.DATABASE_URL,
+  // database: process.env.DATABASE_URL,
 
   // The secret should be set to a reasonably long random string.
   // It is used to sign cookies and to sign and encrypt JSON Web Tokens, unless
@@ -89,27 +92,42 @@ export default NextAuth({
   // pages is not specified for that route.
   // https://next-auth.js.org/configuration/pages
   pages: {
-    // signIn: '/auth/signin',  // Displays signin buttons
-    // signOut: '/auth/signout', // Displays form with sign out button
-    // error: '/auth/error', // Error code passed in query string as ?error=
-    // verifyRequest: '/auth/verify-request', // Used for check email page
-    // newUser: null // If set, new users will be directed here on first sign in
+    signIn: '/auth/signin',  // Displays signin buttons
+    signOut: '/auth/signout', // Displays form with sign out button
+    error: '/auth/error', // Error code passed in query string as ?error=
+    verifyRequest: '/auth/verify-request', // Used for check email page
+    newUser: null // If set, new users will be directed here on first sign in
   },
 
   // Callbacks are asynchronous functions you can use to control what happens
   // when an action is performed.
   // https://next-auth.js.org/configuration/callbacks
   callbacks: {
-    // async signIn(user, account, profile) { return true },
-    // async redirect(url, baseUrl) { return baseUrl },
-    // async session(session, user) { return session },
-    // async jwt(token, user, account, profile, isNewUser) { return token }
+    // async signIn(user, account, profile) { 
+    //   // console.log("PROFILE", profile)
+    //   return true },
+    // async redirect(url, baseUrl) { 
+    //   console.log("baseUrl", baseUrl, "url", url)
+    //   return baseUrl },
+    // // async session(session, user) { 
+    // //   console.log("session", session, user) 
+    // //   return session },
+    // async jwt(token, user, account, profile, isNewUser) { 
+    //   // console.log("jwt token", token, user, account, profile, isNewUser)
+    //   return token }
   },
 
   // Events are useful for logging
   // https://next-auth.js.org/configuration/events
-  events: {},
+  events: {
+    // async signIn(message) { /* on successful sign in */ },
+    // async signOut(message) { /* on signout */ },
+    // async createUser(message) { /* user created */ },
+    // async linkAccount(message) { /* account linked to a user */ },
+    // async session(message) {console.log("session event", message)},
+    // async error(message) { /* error in authentication flow */ }
+  },
 
   // Enable debug messages in the console if you are having problems
-  debug: false,
+  debug: true,
 })
